1a9a5b214818e3a324658276ef843af153429f1b
Eric Liu <ericliu1602@gmail.com>
2025-12-18 14:45:34 -0500

    setup configs for ubuntu

---
+++ install-scripts/kmonad.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo wget https://github.com/kmonad/kmonad/releases/download/0.4.4/kmonad -P /usr/bin/
+
+sudo chmod +x /usr/bin/kmonad
--- install-scripts/nvim.sh
+++ install-scripts/nvim.sh
@@ -5,8 +5,10 @@ sudo apt-get install ripgrep fdfind
 
 # pinned to 0.11.5
 sudo wget -O nvim -P /usr/bin/ https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-linux-x86_64.appimage
+sudo chmod +x nvim
+sudo mv nvim /usr/bin/
 
 git submodule update --init --recursive
 
-ln -sr ../kickstart-modular.nvim/ ~/.config/nvim
+ln -sr ~/dotfiles/kickstart-modular.nvim/ ~/.config/nvim
 
